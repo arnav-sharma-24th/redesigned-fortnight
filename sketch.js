5c94c98decfa05dfb098bac8e2a1f527f5371217
arnav-sharma-24th <71770733+arnav-sharma-24th@users.noreply.github.com>
2021-01-18 23:37:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var PLAY=1
+var END=0;
+var gameState=PLAY
+var score
+var backround,back;
+
+function setup() {
+  createCanvas(800,400);
+  
+}
+
+function draw() {
+  background("black");  
+  drawSprites();
+}
